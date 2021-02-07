830bf1b6a28e4d7535ab064f3e7136e6b69caf0f
Mateo Carriquí <mateocarriqui7@gmail.com>
2021-02-07 14:18:10 -0300

    Added more details to MediaDetails

--- src/queries/Media/MediaDetails/index.ts
+++ src/queries/Media/MediaDetails/index.ts
@@ -1,76 +1,35 @@
 export function ANIME_DETAILS(id: number) {
   return {
     query: `
-      query ($id: Int) {
-        Media(id: $id) {
-          description
-          duration
-          countryOfOrigin
-          synonyms
-          mediaListEntry{
-            id
-            status
-            score
-            progress
-          }
-          tags {
-            id
-            name
-          }
-          nextAiringEpisode {
-            airingAt
-            episode
-          }
-          characters {
-            nodes {
-              id
-              name {
-                full
-              }
-              image {
-                large
-              }
-            }
-          }
-          studios {
-            nodes {
-              id
-              name
-            }
-          }
-          relations {
-            edges {
-              relationType
-              id
-              node {
-                title {
-                  userPreferred
-                }
-                type
-                format
-                status
-                season
-                seasonYear
-              }
-            }
-          }
-        }
-      }
-      
-          
-      `,
-    variables: {
-      id,
-    },
-  };
-}
-
-export function MANGA_DETAILS(id: number) {
-  return {
-    query: `
     query ($id: Int) {
       Media(id: $id) {
+        title {
+          userPreferred
+          english
+          native
+          romaji
+        }
+        coverImage {
+          extraLarge
+        }
+        format
+        status(version: 2)
         description
+        episodes
+        averageScore
+        isAdult
+        genres
+        bannerImage
+        trailer {
+          site
+          id
+          thumbnail
+        }
+        startDate {
+          year
+          month
+          day
+        }
         duration
         countryOfOrigin
         synonyms
@@ -79,7 +38,6 @@ export function MANGA_DETAILS(id: number) {
           status
           score
           progress
-          progressVolumes
         }
         tags {
           id
@@ -119,7 +77,88 @@ export function MANGA_DETAILS(id: number) {
       }
     }
     
-      
+      `,
+    variables: {
+      id,
+    },
+  };
+}
+
+export function MANGA_DETAILS(id: number) {
+  return {
+    query: `
+    query ($id: Int) {
+      Media(id: $id) {
+        title {
+          userPreferred
+          english
+          native
+          romaji
+        }
+        coverImage {
+          extraLarge
+        }
+        format
+        status(version: 2)
+        description
+        episodes
+        averageScore
+        isAdult
+        genres
+        bannerImage
+        trailer {
+          site
+          id
+          thumbnail
+        }
+        startDate {
+          year
+          month
+          day
+        }
+        duration
+        countryOfOrigin
+        synonyms
+        mediaListEntry {
+          id
+          status
+          score
+          progress
+          progressVolumes
+        }
+        tags {
+          id
+          name
+        }
+        characters {
+          nodes {
+            id
+            name {
+              full
+            }
+            image {
+              large
+            }
+          }
+        }
+        relations {
+          edges {
+            relationType
+            id
+            node {
+              title {
+                userPreferred
+              }
+              type
+              format
+              status
+              season
+              seasonYear
+            }
+          }
+        }
+      }
+    }     
       `,
     variables: {
       id,
